12bb0fa39cc68b874195e3fe6c8080d8e9349d2f
Lee Joo Yeop <leejy775216@gmail.com>
2023-07-17 20:38:50 +0900

    수도권 지도

--- uber.py
+++ uber.py
@@ -2,18 +2,20 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
+def main():
+    st.title('서울, 수도권, 경기도')
+    
+    # 서울의 위도와 경도
+    seoul_coordinates = (37.5665, 126.9780)
+    
+    # 위도와 경도를 담은 데이터프레임 생성
+    data = pd.DataFrame({'LATITUDE': [seoul_coordinates[0]], 'LONGITUDE': [seoul_coordinates[1]]})
+    
+    # st.map()을 사용하여 지도를 출력
+    st.map(data, zoom=9)
 
-st.subheader('Raw data')
-st.write(data)
+if __name__ == '__main__':
+    main()
+
